757488e00a9dfeaa68ee296da5afdb0430db52d0
damiano-g <114597691+damiano-g@users.noreply.github.com>
2025-08-11 21:54:52 +0200

    Variables created. Naming validation function

--- m2/es1/script.js
+++ m2/es1/script.js
@@ -1,9 +1,21 @@
-function addClass(){
+const inBox = document.querySelectorAll("input[type='text']");
+const naming = document.querySelectorAll(".fullName");
+const email = document.querySelector("#email");
+const pass = document.querySelector("#pass");
+const confirmPass = document.querySelector("#confirmPass");
+const subBtn = document.querySelector("input[type='submit']");
 
-    let inputBoxes = document.querySelectorAll("input[type='text']");
-    for(let i=0; i < inputBoxes.length; i++){
-        inputBoxes[i].classList.add("valid");
-    }
+function validNaming(event) {
+        
+        if(String(event.target.value).length >= 2){
+                event.target.classList.add("valid");
+                event.target.classList.remove("invalid");        
+        }else{
+                event.target.classList.add("invalid");
+                event.target.classList.remove("valid");
+        }
 }
 
-window.addEventListener("load", addClass);
+subBtn.disabled = true;
+
+naming.forEach(input => input.addEventListener("input", validNaming));
