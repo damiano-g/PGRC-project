eb557fdd28da3210c8ac7e0a5927f85f376b7cfa
damiano-g <114597691+damiano-g@users.noreply.github.com>
2025-08-13 16:40:45 +0200

    Code formatting

--- m2/es2/script.js
+++ m2/es2/script.js
@@ -1,7 +1,5 @@
 const jobTitle = document.querySelectorAll(".jobTitle");
 
-
-
 jobTitle.forEach(item => item.addEventListener("mouseover", event => document.body.style.cursor = "pointer"));
 jobTitle.forEach(item => item.addEventListener("mouseout", event => document.body.style.cursor = "auto"));
 jobTitle.forEach(item => item.addEventListener("click", event => event.target.nextElementSibling.classList.toggle("hide")));
