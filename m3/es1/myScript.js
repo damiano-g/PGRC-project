ab13352d5d16b79af43d88b4caccba26d270769c
damiano-g <114597691+damiano-g@users.noreply.github.com>
2025-08-14 15:07:13 +0200

    API data retrieval and error handling

--- m3/es1/myScript.js
+++ m3/es1/myScript.js
@@ -1,5 +1,6 @@
 const searchBar = document.getElementById("searchBar");
 let searchContent;
+let contentJSON;
 
 let debounceTimeout;
 
@@ -13,7 +14,21 @@ function delaySearch(){
 function retrieveSearch(){
 
     if(String(searchBar.value) != ""){
+        
         searchContent = String(searchBar.value);
+        
+        //encodeURIComponent evita caratteri che potrebbero compromettere la ricerca
+        let url = `https://api.themoviedb.org/3/search/movie?api_key=571d7713f769aae024b522b1d9231927&query=${encodeURIComponent(searchContent)}&page=1`;
+
+        fetch(url)
+            .then(response => {
+                if(!response.ok){
+                    throw new Error("Errore nella risposta");
+                }
+                return response.json();
+            })
+            .then(response => contentJSON = response)
+            .catch(() => alert("Impossibile effettuare la richiesta"));
     }
 }
 
