21ec65e8b94262a8c8ab238348c760c57665aa6c
damiano-g <114597691+damiano-g@users.noreply.github.com>
2025-09-05 10:44:50 +0200

    Reapply "Personal page fav btn working"
    This reverts commit 14f3c90a2623c7362b7e7495715037f63b018d17.

--- js/pages-scripts/favourites.js
+++ js/pages-scripts/favourites.js
@@ -14,9 +14,10 @@
 
 import { createPreviewArray } from "../data-models.js";
 import { fetchById } from "../recipesAPI.js";
+import { RecipeStatus } from "../recipesManagement.js";
 import { getStoredReviews, isReviewedBy, GlobalRatingFunctions, UserRatingFunctions } from "../reviewsManagement.js";
-import { DisplayPreviews } from "../UI.js";
-import { getRegisteredUsers, getLoggedUserId, searchUserById } from "../usersManagement.js";
+import { DisplayPreviews, favBtnDisplay } from "../UI.js";
+import { getRegisteredUsers, getLoggedUserId, searchUserById, currentUserFavourite, updateUserFavourites } from "../usersManagement.js";
 
 // ================================================================================================
 // DOM ELEMENTS
@@ -88,11 +89,17 @@ const personalPageBody = document.querySelector("body");
 personalPageBody.addEventListener("click", (click) => {
     /** @type {HTMLElement|null} Card element closest al target del click */
     const card = click.target.closest(".card");
+    const isBtn = click.target.matches(".fav-icon");
 
-    if(card){
+    if(card && !isBtn){
         // Naviga alla pagina dettagli passando l'ID della ricetta come query parameter
         window.location.href = `../../pages/recipe-details.html?id=${card.dataset.itemId}`;
-    }
+    };
+
+    if(isBtn){
+        updateUserFavourites(card.dataset.itemId);
+        favBtnDisplay(card.querySelector(".fav-icon"), Boolean(getLoggedUserId()), currentUserFavourite(card.dataset.itemId));
+    };
 });
 
 /**
@@ -155,7 +162,7 @@ window.addEventListener("load", async () => {
      * Array filtrato di review dell'utente corrente per estrazione recipe IDs
      * @type {Array<Object>} 
      */
-    const currentUserRevsRecipesIds = getStoredReviews().filter(element => isReviewedBy(element.recipeId, currentUser.id));
+    const currentUserRevsRecipesIds = getStoredReviews().filter(element => RecipeStatus.isReviewed(element.recipeId));
 
     // ============================================================================================
     // SEZIONE FAVOURITES
