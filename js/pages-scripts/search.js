dbd0a57ce08c02072f707a06c19d166c79a6753b
damiano-g <114597691+damiano-g@users.noreply.github.com>
2025-09-07 15:50:17 +0200

    Fixed authentication logic

--- js/pages-scripts/search.js
+++ js/pages-scripts/search.js
@@ -74,8 +74,12 @@ resultsContainer.addEventListener("click", (click) => {
     };
 
     if(isBtn){
-        LoggedUser.updateFavourites(card.dataset.itemId);
-        favBtnDisplay(card.querySelector(".fav-icon"), card.dataset.itemId);
+        if(LoggedUser.isLogged()){
+            LoggedUser.updateFavourites(card.dataset.itemId);
+            favBtnDisplay(card.querySelector(".fav-icon"), card.dataset.itemId);
+        }else{
+            window.location.href = "./login.html";
+        };
     };
 });
 
